a1dc6577c2420d8c33529b6c9d71d7e2c00f01d2
Jordan Hudgens <jordan@devcamp.com>
2017-06-26 14:39:55 +0000

    Added placeholder images method

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -10,7 +10,7 @@ class Portfolio < ApplicationRecord
   after_initialize :set_defaults
   
   def set_defaults
-    self.main_image ||= "http://placehold.it/600x400"
-    self.thumb_image ||= "http://placehold.it/350x200" 
+    self.main_image ||= Placeholder.image_generator(height: '400', width: '600')
+    self.thumb_image ||= Placeholder.image_generator(height: '200', width: '350')
   end
 end
