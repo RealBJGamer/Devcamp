69bfe79d84bf42a7964272dd30eaf713e286ad5f
Jordan Hudgens <jordan@devcamp.com>
2017-06-26 17:32:42 +0000

    Form for nested attributes

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,6 +1,7 @@
 class Portfolio < ApplicationRecord
   has_many :technologies
-  accepts_nested_attributes_for :technologies
+  accepts_nested_attributes_for :technologies,
+                                reject_if: lambda {|attrs| attrs['name'].blank?}
   validates_presence_of :title , :body , :main_image , :thumb_image, :main_image
 
   def self.angular
@@ -12,7 +13,7 @@ class Portfolio < ApplicationRecord
   after_initialize :set_defaults
   
   def set_defaults
-    self.main_image ||= Placeholder.image_generator(height: '400', width: '600')
-    self.thumb_image ||= Placeholder.image_generator(height: '200', width: '350')
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
   end
 end
