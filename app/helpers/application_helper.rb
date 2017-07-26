779f574338cfbaee03a28e0873af0a82d5b1bb9a
Jordan Hudgens <jordan@devcamp.com>
2017-07-26 14:56:26 +0000

    Updated

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,8 @@ module ApplicationHelper
     " ".html_safe +
     (link_to "Login", new_user_session_path, class: style)
     else
-    link_to "Logout", destroy_user_session_path, method: :delete, class: style
+    (link_to "Logout", destroy_user_session_path, method: :delete, class: style)+
+    " ".html_safe + (link_to "Edit User", edit_user_registration_path, class: style)
     end
   end
   
