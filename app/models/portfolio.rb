6607fa6c583ba66994bcb05bb98495805a1aab7f
Jordan Hudgens <jordan@devcamp.com>
2017-08-01 21:47:42 +0000

    Updated

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -10,7 +10,14 @@ class Portfolio < ApplicationRecord
 
   def self.angular
     where(subtitle: 'Angular')
+    
+  end 
+  
+  def self.by_position
+    order("position ASC")
   end
+  
+  
   
   scope :ruby_on_rails_porfolio_items, -> { where(subtitle: 'Ruby on Rails') }
   
