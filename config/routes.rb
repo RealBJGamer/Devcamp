d8573b78aa70bf59e705fdbc5d2e29b2f600c5d6
Jordan Hudgens <jordan@devcamp.com>
2017-08-06 01:47:03 +0000

    Updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :comments
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
  resources :portfolios, except: [:show] do
    put :sort, on: :collection
@@ -8,7 +9,7 @@ Rails.application.routes.draw do
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-
+  get 'news', to: 'pages#news'
   resources :blogs do
     member do
       get :toggle_status
